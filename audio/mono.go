cc16a7e70361f2a3e05c20c5bb20d522dee15edc
Sianwa Atemi <sa.atemi@gmail.com>
2026-08-17 21:37:53 +0300

    stereo to mono conversion

---
+++ audio/mono.go
@@ -0,0 +1,32 @@
+package audio
+
+import "fmt"
+
+func ToMono(wav *WAVFile) ([]int16, error) {
+	if wav.NumChannels == 1 {
+		return wav.Samples, nil
+	}
+
+	if len(wav.Samples)%2 != 0 {
+		return nil, fmt.Errorf("corrupted wav file")
+	}
+
+	monoSamples := len(wav.Samples) / 2
+	mono := make([]int16, monoSamples)
+
+	i := 0
+	j := 0
+	for {
+		if j == monoSamples {
+			break
+		}
+		left := int32(wav.Samples[i])
+		right := int32(wav.Samples[i+1])
+		res := (left + right) / 2
+		mono[j] = int16(res)
+		i = i + 2
+		j++
+	}
+
+	return mono, nil
+}
--- main.go
+++ main.go
@@ -19,9 +19,15 @@ func main() {
 		log.Fatal(err)
 	}
 
+	mono, err := audio.ToMono(wav)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	log.Printf("Sample Rate: %d", wav.SampleRate)
 	log.Printf("Num Channels: %d", wav.NumChannels)
 	log.Printf("Bits Per Sample: %d", wav.BitsPerSample)
 	log.Printf("Num Samples Len: %v", len(wav.Samples))
+	log.Printf("Mono Len: %v", len(mono))
 
 }
