e9f1d9e90d884b074959859d4b0935db0ea983dc
Sianwa Atemi <sa.atemi@gmail.com>
2026-09-02 15:18:32 +0300

    packed hashes to a small 32bit int

--- fingerprint/hash.go
+++ fingerprint/hash.go
@@ -1,11 +1,12 @@
 package fingerprint
 
 const (
-	zoneSize = 5
+	zoneSize     = 5
+	anchorOffset = 3
 )
 
 type Hash struct {
-	Address    [3]int
+	Address    uint32
 	AnchorTime int
 }
 
@@ -13,7 +14,7 @@ func Hashing(peaks []Peak) []Hash {
 	hashes := make([]Hash, 0)
 
 	for i := 0; i+zoneSize <= len(peaks); i++ {
-		anchorIndex := i - 3
+		anchorIndex := i - anchorOffset
 		if anchorIndex < 0 {
 			continue
 		}
@@ -23,8 +24,10 @@ func Hashing(peaks []Peak) []Hash {
 
 		for _, point := range zone {
 			delta := point.Time - anchor.Time
+			address := packAddress(anchor.Frequency, point.Frequency, delta)
+
 			hashes = append(hashes, Hash{
-				Address:    [3]int{anchor.Frequency, point.Frequency, delta},
+				Address:    address,
 				AnchorTime: anchor.Time,
 			})
 		}
@@ -32,4 +35,12 @@ func Hashing(peaks []Peak) []Hash {
 	}
 
 	return hashes
+}
+
+func packAddress(anchorFrequency, pointFrequency, deltaTime int) uint32 {
+	af := uint32(anchorFrequency) & 0x1FF // 9 bits
+	pf := uint32(pointFrequency) & 0x1FF  // 9 bits
+	dt := uint32(deltaTime) & 0x3FFF      // 14 bits (0x3FFF = 16383)
+
+	return af<<23 | pf<<14 | dt
 }
